deea48f5ae0f2d6d5788764197f5ff3150eda6fb
dannyknows <dannylow465@gmail.com>
2020-05-03 17:43:58 +1000

    cleaned up grammar on books page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,47 @@
 class BooksController < ApplicationController
   def books
-    @books = Book.all
+    @books = Book.order('title asc').all
+    helpers.helpme
+  end
+
+  def authors
+    @authors = Author.order('name asc').all
+  end
+
+  def genres
+    @genres = Genre.order('name asc').all
   end
 
   def show
     @book = Book.find(params[:id])
+  end
+  
+  def edit 
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    bookgenre = BooksGenre.all
+    i = 0
+    bookgenre.each do |connection|
+      if connection.book_id == params[:id].to_i
+        connection.genre_id = genre_params(i)["genre_id_#{i}"].to_i
+        connection.save
+        i += 1
+      end
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
+
+  def genre_params(num)
+    params.require(:book).permit("genre_id_#{num}")
   end
 
 end
